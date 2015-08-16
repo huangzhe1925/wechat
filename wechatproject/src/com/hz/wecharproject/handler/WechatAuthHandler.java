a68d6376c80d0ce1d978171c6511a8e8ace4cd8f
Administrator <Administrator@PC-20150711UNAR>
2015-08-16 15:47:44 +0800

    fds

--- wechatproject/src/com/hz/wecharproject/handler/WechatAuthHandler.java
+++ wechatproject/src/com/hz/wecharproject/handler/WechatAuthHandler.java
@@ -1,7 +1,11 @@
 package com.hz.wecharproject.handler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,16 +17,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WechatAuthHandler {
 
+	@RequestMapping(value = "wechatAuth")
+	@ResponseBody
+	public String wechatAuth(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		/** 读取接收到的xml消息 */
+		StringBuffer sb = new StringBuffer();
+		InputStream is = request.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		String s = "";
+		while ((s = br.readLine()) != null) {
+			sb.append(s);
+		}
+		String xml = sb.toString(); // 次即为接收到微信端发送过来的xml数据
+
+		String result = "";
+		/** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */
+		String echostr = request.getParameter("echostr");
+		if (echostr != null && echostr.length() > 1) {
+			result = echostr;
+		} else {
+			// 正常的微信处理流程
+		}
+		return result;
+	}
+
 	@RequestMapping(value = "showBody")
 	@ResponseBody
 	public String showBody(HttpServletRequest request,
 			HttpServletResponse response) {
-			response.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 
 		int size = request.getContentLength();
 
 		InputStream is;
-		String res="";
+		String res = "";
 		try {
 			is = request.getInputStream();
 			byte[] reqBodyBytes = readBytes(is, size);
