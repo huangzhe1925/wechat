a9784e604ff77b949a0d609d3beb5f30550c16c3
Administrator <Administrator@PC-20150711UNAR>
2015-08-16 15:02:06 +0800

    fds

---
+++ wechatproject/src/com/hz/wecharproject/handler/GeneralController.java
@@ -0,0 +1,22 @@
+package com.hz.wecharproject.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller  
+public class GeneralController {  
+  
+    @RequestMapping(value = "testJson")  
+    @ResponseBody  
+    public Map<String, String> addUser() {  
+        Map<String, String> map = new HashMap<String, String>(1);  
+        map.put("success", "true");  
+        return map;  
+    }  
+  
+}  
