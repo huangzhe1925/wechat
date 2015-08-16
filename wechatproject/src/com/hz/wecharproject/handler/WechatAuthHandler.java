b5a5ea28c0103f0ccb16c9c6db4a4598b2c6eebb
Administrator <Administrator@PC-20150711UNAR>
2015-08-16 16:43:25 +0800

    dd

--- wechatproject/src/com/hz/wecharproject/handler/WechatAuthHandler.java
+++ wechatproject/src/com/hz/wecharproject/handler/WechatAuthHandler.java
@@ -18,25 +18,25 @@ public class WechatAuthHandler {
 			HttpServletResponse response) throws Exception {
 		response.setCharacterEncoding("UTF-8");
 
-		 // 次即为接收到微信端发送过来的xml数据
+		// 次即为接收到微信端发送过来的xml数据
 
 		String result = "";
 		/** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */
 		String echostr = request.getParameter("echostr");
 		if (echostr != null && echostr.length() > 1) {
-			if(WechatUtil.checkSignature(request)){  
-                return echostr;  
-            }else{
-            	System.out.println("Error checkSignature");
-            	return "Error";  
-            }  
+			if (WechatUtil.checkSignature(request)) {
+				return echostr;
+			} else {
+				System.out.println("Error checkSignature");
+				return "Error";
+			}
 		} else {
 			// 正常的微信处理流程
-			 System.out.print(WechatUtil.getXMLFromRequest(request)); 
+			result = WechatUtil.getXMLFromRequest(request);
+			System.out.println(result);
+			System.out.println();
 		}
 		return result;
 	}
-	
-	
 
 }
