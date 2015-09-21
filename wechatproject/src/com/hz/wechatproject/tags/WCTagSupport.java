d9d3a7094e0da019e6c0c215c900735933e2c8d2
roger huang <562918024@qq.com>
2015-09-21 15:34:13 +0800

    ggg

--- wechatproject/src/com/hz/wechatproject/tags/WCTagSupport.java
+++ wechatproject/src/com/hz/wechatproject/tags/WCTagSupport.java
@@ -1,8 +1,8 @@
 package com.hz.wechatproject.tags;
 
-import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.tagext.BodyTagSupport;
 
-public abstract class WCTagSupport extends TagSupport {
+public abstract class WCTagSupport extends BodyTagSupport {
 
 	private static final long serialVersionUID = -3285542123484301362L;
 
