61179fd63a4547c4aa2f57e5a79f365db34d0c4c
zhe.a.huang <zhe.a.huang@CDC313124.dir.svc.accenture.com>
2015-10-21 10:53:54 +0800

    fff

--- wechatproject/src/com/hz/wechatproject/utils/DBConnections.java
+++ wechatproject/src/com/hz/wechatproject/utils/DBConnections.java
@@ -8,22 +8,14 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 
+@Deprecated
 @Component
 public class DBConnections {
 	
 	private static Logger logger=Logger.getLogger(DBConnections.class);
 
-	static{
-		logger.debug("org.sqlite.JDBC");
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			logger.error(e);
-		}
-	}
-	
 	public Connection getDBConnection() throws SQLException{
-		Connection conn = DriverManager.getConnection(PropertiesUtil.getProperty(PropertiesUtil.DBSOURCE_SQLITE_URL));
+		Connection conn = DriverManager.getConnection(PropertiesUtil.getStringProperty(PropertiesUtil.DBSOURCE_SQLITE_URL));
 		return conn;
 	}
 }
--- wechatproject/src/com/hz/wechatproject/utils/PropertiesUtil.java
+++ wechatproject/src/com/hz/wechatproject/utils/PropertiesUtil.java
@@ -1,47 +1,64 @@
 package com.hz.wechatproject.utils;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.log4j.Logger;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class PropertiesUtil {
-	
-	private static Logger logger=Logger.getLogger(PropertiesUtil.class);
-	
-	public static final String DBSOURCE_SQLITE_URL="dbsource.sqlite.url";
-	
-	private static Properties deployInfoPro;
-	private static final String PROPERTY_FILE_NAME="wechat.properties";  
-	
+
+	private static Logger logger = Logger.getLogger(PropertiesUtil.class);
+
+	private static PropertiesConfiguration propertiesConfiguration;
+
+	public static final String PROPERTY_FILE_NAME = "wechat.properties";
+
+	public static final String DBSOURCE_SQLITE_URL = "dbsource.sqlite.url";
+
+
 	static {
-		deployInfoPro = new Properties();
+		loadProperties();
+	}
+	
+
+	public static void loadProperties() {
 		try {
-			String path = PropertiesUtil.class.getResource("/").getPath();
-			path = path.substring(1, path.indexOf("classes"));
-			deployInfoPro.load(new FileInputStream(path+ PROPERTY_FILE_NAME));
+			Resource resource = new ClassPathResource(PROPERTY_FILE_NAME);
+			URL resourceURL;
+			resourceURL = resource.getURL();
+			propertiesConfiguration = new PropertiesConfiguration(resourceURL);
+			propertiesConfiguration.setReloadingStrategy(new FileChangedReloadingStrategy());
 		} catch (Exception e) {
 			logger.error(e);
+			propertiesConfiguration = new PropertiesConfiguration();
 		}
 	}
 
-	public static void refreshProperties() {
-		deployInfoPro = new Properties();
-		try {
-			String path = PropertiesUtil.class.getResource("/").getPath();
-			path = path.substring(1, path.indexOf("classes"));
-			deployInfoPro.load(new FileInputStream(path+ PROPERTY_FILE_NAME));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static PropertiesConfiguration getPropertiesConfiguration() {
+		return propertiesConfiguration;
 	}
 
-	public static Properties getProperties() {
-		return deployInfoPro;
-	}
-
-	public static String getProperty(String key) {
-		return deployInfoPro.getProperty(key, "");
+	public static String getStringProperty(String key) {
+		return propertiesConfiguration.getString(key);
 	}
 	
+	public static Integer getIntProperty(String key) {
+		return propertiesConfiguration.getInteger(key, null);
+	}
+	
+	public static Object getObjProperty(String key){
+		return propertiesConfiguration.getInteger(key, null);
+	}
+	
+	
+	public static void main(String args[]){
+		System.out.println(PropertiesUtil.getStringProperty(DBSOURCE_SQLITE_URL));
+	}
+
 }
